f041ffa1977507963294fcfad2340260fb12980a
hsmukesh <159506763+hsmukesh@users.noreply.github.com>
2024-04-12 11:49:44 +0530

    Update Exp1_Logicgates.v

--- Exp1_Logicgates.v
+++ Exp1_Logicgates.v
@@ -1,1 +1,11 @@
-
+module allgates(a,b,y1,y2,y3,y4,y5,y6,y7);
+input a,b;
+output y1,y2,y3,y4,y5,y6,y7;
+and g1(y1,a,b);
+or  g2(y2,a,b);
+not g3(y3,a);
+xor g4(y4,a,b);
+xnor g5(y5,a,b);
+nand g6(y6,a,b);
+nor g7(y7,a,b);
+endmodule
