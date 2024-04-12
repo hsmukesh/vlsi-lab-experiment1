8934c042e2a9708eb598283725f46175c692c797
hsmukesh <159506763+hsmukesh@users.noreply.github.com>
2024-04-12 11:47:45 +0530

    Update Exp1_Fulladder.v

--- Exp1_Fulladder.v
+++ Exp1_Fulladder.v
@@ -1,1 +1,10 @@
-
+module fulladder(a,b,c,sum,carry);
+input a,b,c;
+output sum,carry;
+wire w1,w2,w3;
+xor g1(w1,a,b);
+xor g2(sum,w1,c);
+and g3(w2,a,b);
+and g4(w3,w1,c);
+or g5(carry,w3,w2);
+endmodule
