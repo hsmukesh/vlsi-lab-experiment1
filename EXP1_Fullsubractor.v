612389bbfdae73c1aa39cdcbcba8e41e6a779775
hsmukesh <159506763+hsmukesh@users.noreply.github.com>
2024-04-12 11:48:21 +0530

    Update EXP1_Fullsubractor.v

--- EXP1_Fullsubractor.v
+++ EXP1_Fullsubractor.v
@@ -1,1 +1,12 @@
-
+module fullsub(a,b,c,difference,borrow);
+input a,b,c;
+output difference,borrow;
+wire w1,w2,w4,w5,w6;
+xor x1(w2,a,b);
+xor x2(difference,w2,c);
+not n1(w1,a);
+and a1(w5,w1,b);
+not n2(w4,w2);
+and a2(w6,w4,c);
+or o1(borrow,w5,w6);
+endmodule
